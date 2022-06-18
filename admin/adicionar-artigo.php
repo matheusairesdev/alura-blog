33598d51e5616ca48f2de54cada6470bdc748758
Matheus Aires <matheus.aiires@gmail.com>
2022-06-18 19:47:16 -0300

    Projeto Concluído

--- admin/adicionar-artigo.php
+++ admin/adicionar-artigo.php
@@ -1,3 +1,18 @@
+<?php
+
+require '../config.php';
+require '../src/Artigo.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $artigo = new Artigo($mysql);
+    $artigo->adicionar($_POST['titulo'], $_POST['conteudo']);
+
+    header('Location: /alura-blog/admin/');
+    die();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,7 +25,7 @@
 <body>
     <div id="container">
         <h1>Adicionar Artigo</h1>
-        <form action="adicionar-artigo.html" method="post">
+        <form action="adicionar-artigo.php" method="post">
             <p>
                 <label for="">Digite o título do artigo</label>
                 <input class="campo-form" type="text" name="titulo" id="titulo" />
